8ed461447b9b88c975ca76d10493e67b31ad7ee7
osmancelikok <45890310+osmancelikok@users.noreply.github.com>
2021-04-06 08:52:40 -0500

    Commit

---
+++ Assets/Ring.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ring : MonoBehaviour
+{
+    PlayerCollider pc;
+    public int SumArray;
+    public int SumArrayOld;
+
+    void Start()
+    {
+      pc = FindObjectOfType<PlayerCollider>();
+      SumArray = pc.SumArray;
+      SumArrayOld = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
